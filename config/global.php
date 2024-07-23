43ff4344c1bc96ab456e74dbc2c96668fc7bb1ee
GarciaOliver <oliver.garcia810@ist17dejulio.edu.ec>
2024-07-23 06:38:43 -0500

    Estructura de carpetas

---
+++ config/global.php
@@ -0,0 +1,18 @@
+<?php 
+//ip de la pc servidor base de datos
+define("DB_HOST", "localhost");
+
+// nombre de la base de datos
+define("DB_NAME", "GARCIA4to");
+
+//nombre de usuario de base de datos
+define("DB_USERNAME", "root");  //GARCIA
+
+//conraseña del usuario de base de datos
+define("DB_PASSWORD", "bajoterra100");  //Garcia.2024
+
+//codificacion de caracteres
+define("DB_ENCODE", "utf8");
+
+ 
+ ?>
